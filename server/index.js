7abd3b90567b12c6c2d21721b1de0d85f0ae6862
Lotan Toiw <95045934+LotanT@users.noreply.github.com>
2022-12-14 14:41:19 +0200

    try to fix error

--- server/index.js
+++ server/index.js
@@ -6,16 +6,15 @@ const path = require('path')
 const app = express()
 app.use(express.json())
 app.use(cors())
-app.use(express.static('public'));
 
 const server = http.createServer(app)
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.resolve(__dirname, 'public')));
-  } else {
-    const corsOptions = {
-      origin: [
-        'http://127.0.0.1:3000',
+  app.use(express.static(path.resolve(__dirname, 'public')));
+} else {
+  const corsOptions = {
+    origin: [
+      'http://127.0.0.1:3000',
         'http://localhost:3000',
         '*'
       ],
@@ -24,22 +23,25 @@ if (process.env.NODE_ENV === 'production') {
     app.use(cors(corsOptions));
   }
 
-const { connectSockets } = require('./services/socket.service');
-const { startAllListeners } = require('./services/listeners.service')
-const chatRoutes = require('./api/chat/chat.routes')
-const eventsRoutes = require('./api/events/events.routes')
-const playerRoutes = require('./api/players/players.routes')
+  const { connectSockets } = require('./services/socket.service');
+  const { startAllListeners } = require('./services/listeners.service')
+  const chatRoutes = require('./api/chat/chat.routes')
+  const eventsRoutes = require('./api/events/events.routes')
+  const playerRoutes = require('./api/players/players.routes')
 
-app.use('/api/chat', chatRoutes)
-app.use('/api/events', eventsRoutes)
+  app.use('/api/chat', chatRoutes)
+  app.use('/api/events', eventsRoutes)
 app.use('/api/players', playerRoutes)
 connectSockets(server)
 startAllListeners()
 
 const port = process.env.PORT || 3001;
 
+const root = path.join(__dirname, 'public')
+app.use(express.static(root));
+
 app.get('/**', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'))
+  res.sendFile('index.html', {root})
 })
 
 server.listen(port, ()=>{
